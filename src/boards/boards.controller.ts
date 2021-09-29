868c3f9a34bd76447faf6ae1abf58b72c8f08dd5
jae <wogud1514@naver.com>
2021-09-29 20:43:19 +0900

    기본 crud

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
-import { Board } from './board.model';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -11,4 +12,30 @@ export class BoardsController {
   getAllBoards() : Board[] {
       return this.boardsService.getAllBoards();
   }
+
+  @Post()
+  createBoard(
+    @Body() createBoardDto: CreateBoardDto
+  ): Board {
+    return this.boardsService.createBoard(createBoardDto); 
+  }
+
+  @Get('/:id')
+  getBoardId(@Param('id') id: string): Board{
+    return this.boardsService.getBoardById(id)
+  }
+
+  @Delete('/:id')
+  deleteBoardById(@Param('id') id: string): void{
+    this.boardsService.deleteBoardById(id);
+  }
+
+  @Patch('/:id/status')
+  updateBoardService(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus,
+  ){
+    return this.boardsService.updateBoardStatus(id, status);
+  }
+
 }
